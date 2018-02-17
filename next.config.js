1557e6d298daa75321a3699cacfaf406fc35fe37
tridung <tridung141196@gmail.com>
2018-02-18 00:55:14 +0700

    afsdf

--- next.config.js
+++ next.config.js
@@ -1,24 +1,9 @@
-/*
 module.exports = {
   exportPathMap: function() {
     return {
       "/": { page: "/" }
     };
   },
-  assetPrefix: process.env.NODE_ENV === "production" ? "https://tridung141196.github.io/waiting-m2love" : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? "waiting-m2love" : ""
 };
-*/
 
-module.exports = {
-  webpack: (config, { dev }) => {
-    // Perform customizations to config
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  }
-}
