48d221346fea265a78ab93f35beddaefc5bf262c
tridung <tridung141196@gmail.com>
2018-02-18 00:49:58 +0700

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   exportPathMap: function() {
     return {
@@ -6,3 +7,18 @@ module.exports = {
   },
   assetPrefix: process.env.NODE_ENV === "production" ? "https://tridung141196.github.io/waiting-m2love" : ""
 };
+*/
+
+module.exports = {
+  webpack: (config, { dev }) => {
+    // Perform customizations to config
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }
+}
