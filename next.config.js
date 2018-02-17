c87216c3307dc7a0c972f64fcc9164e0d196f7e9
tridung <tridung141196@gmail.com>
2018-02-18 00:39:47 +0700

    add link github

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
       "/": { page: "/" }
     };
   },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/waiting-m2love" : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? "https://tridung141196.github.io/waiting-m2love" : ""
 };
